dd5d200675e66da0ba9fa82f567587bb67e771fa
doston <dos400dos400@gmail.com>
2022-01-12 19:55:10 +0500

    first commit

--- app/src/main/java/uz/hamroev/toshkentshaharxotirakitob/MainActivity.kt
+++ app/src/main/java/uz/hamroev/toshkentshaharxotirakitob/MainActivity.kt
@@ -1,8 +1,13 @@
 package uz.hamroev.toshkentshaharxotirakitob
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import uz.hamroev.toshkentshaharxotirakitob.activity.HomeActivity
 import uz.hamroev.toshkentshaharxotirakitob.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,23 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window?.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide()
+        startIntroUI()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 1500)
+
+
+    }
+
+    private fun startIntroUI() {
+
     }
 }
--- app/src/main/java/uz/hamroev/toshkentshaharxotirakitob/activity/HomeActivity.kt
+++ app/src/main/java/uz/hamroev/toshkentshaharxotirakitob/activity/HomeActivity.kt
@@ -1,16 +1,135 @@
 package uz.hamroev.toshkentshaharxotirakitob.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import uz.hamroev.toshkentshaharxotirakitob.R
 import uz.hamroev.toshkentshaharxotirakitob.databinding.ActivityHomeBinding
+import uz.hamroev.toshkentshaharxotirakitob.fragment.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
     lateinit var binding: ActivityHomeBinding
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var eventFragment: EventFragment
+    lateinit var searchFragment: SearchFragment
+    lateinit var sendFragment: SendFragment
+    lateinit var usersFragment: UsersFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        setSupportActionBar(binding.includeAppBar.toolBar)
+        val actionBar = supportActionBar
+        actionBar?.title = "Toshkent Shahar - Xotira Kitob"
+
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.includeAppBar.toolBar,
+            R.string.open,
+            R.string.close
+        ) {
+
+        }
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+
+        when (menuItem.itemId) {
+
+            R.id.home -> {
+                homeFragment = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, homeFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.event -> {
+                eventFragment = EventFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, eventFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.search -> {
+                searchFragment = SearchFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, searchFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.send -> {
+                sendFragment = SendFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, sendFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.users -> {
+                usersFragment = UsersFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, usersFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.evaluation -> {
+                Toast.makeText(this, "Baholash", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.share -> {
+                Toast.makeText(this, "Ulashish", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.exit -> {
+                finish()
+            }
+
+
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
